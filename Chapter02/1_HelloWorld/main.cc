ac868d2c06ab746942f9b567ea69c305a077dc41
PandaLehre <christian.holz@lehre.dhbw-stuttgart.de>
2024-03-28 19:50:24 +0100

    worked on A1: Einleitung

--- Chapter02/1_HelloWorld/main.cc
+++ Chapter02/1_HelloWorld/main.cc
@@ -1,8 +1,15 @@
 #include <iostream>
 
+// C++: g++ (gcc) Compiler for Windows and Linux
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello World!" << std::endl;
 
     return 0;
 }
+
+// Creat exe. --> g++ main.cc -o main
+//      with Compiler  sourcefile -o targetfile
+
+// Execute exe. --> ./targetfile (LINUX)
+// Execute exe. --> targetfile.exe (WINDOWS)
